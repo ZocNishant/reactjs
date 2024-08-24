c7c7a68ae39ed6a19bb35197cca7bdd7ecec96fd
Zoc Nishant <zocnishant@gmail.com>
2024-08-24 13:57:56 +0545

    .

--- myapp/src/components/State.jsx
+++ myapp/src/components/State.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { useState } from "react";
 
 const State = () => {
-  const [val, setVal] = useState(12);
-
+  const [val, setVal] = useState({ name: "Nishant", isBanned: false });
   return (
-    <div className="m-2">
-      <h1>{val}</h1>
+    <div className="m-3">
+      <h1>name: {val.name}</h1>
+      <h2>banned: {val.isBanned.toString()}</h2>
+
       <button
-        onClick={() => setVal((prev) => prev + 1)}
-        className="bg-blue-500 px-3 py-3 rounded-md"
+        onClick={() => setVal({ ...val, isBanned: !val.isBanned })}
+        className={`${
+          val.isBanned ? "bg-blue-500" : "bg-red-500"
+        } px-3 py-2 rounded-lg text-yellow-100 cursor-pointer hover:bg-orange-500 hover:text-violet-500`}
       >
         Change
       </button>
