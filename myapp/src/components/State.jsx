eeeac7695a0074178985be1fdcc942698cf3f6fe
Zoc Nishant <zocnishant@gmail.com>
2024-08-26 19:55:32 +0545

    .

--- myapp/src/components/State.jsx
+++ myapp/src/components/State.jsx
@@ -1,30 +1,19 @@
 import React from "react";
-import { useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
 
 const State = () => {
-  const [val, setVal] = useState([
-    { name: "Nishant", age: 22 },
-    { name: "Nishan", age: 21 },
-    { name: "Nisha", age: 33 },
-  ]);
   return (
-    <div className="p-5">
-      {val.map((item) => (
-        <>
-          <h1>{item.name}</h1>
-          <h1>{item.age}</h1>
-        </>
-      ))}
-      <button
-        onClick={() =>
-          val.map((item) =>
-            item.name === "Nishant" ? { name: "Nishant", age: 20 } : item
-          )
-        }
-        className="px-3 py-2 rounded-lg bg-green-500 cursor-pointer "
-      >
-        Change
-      </button>
+    <div className="w-full h-screen bg-zinc-300 flex justify-center items-center">
+      <div className="relative w-60 h-32 rounded-lg bg-zinc-700">
+        <img
+          className="w-full h-full object-cover overflow-hidden rounded-lg"
+          src="https://images.unsplash.com/photo-1724277228191-0cf4c80ee56f?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <span className="absolute cursor-pointer flex rounded-full bg-[#dadada8b] items-center justify-center w-8 h-8 bottom-[0%] left-1/2 -translate-x-[50%] -translate-y-[50%]">
+          <FaArrowRight size={"0.8em"} />
+        </span>
+      </div>
     </div>
   );
 };
