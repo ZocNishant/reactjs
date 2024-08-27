7411df56dcadfd592aeda5c4cd284cf45ff48527
Zoc Nishant <zocnishant@gmail.com>
2024-08-27 07:55:06 +0545

    .

--- myapp/src/components/State.jsx
+++ myapp/src/components/State.jsx
@@ -1,18 +1,26 @@
 import React from "react";
-import { FaArrowRight } from "react-icons/fa";
 
-const State = () => {
+const State = ({ values, handleClick }) => {
+  const { name, profession, image } = values;
+
   return (
-    <div className="w-full h-screen bg-zinc-300 flex justify-center items-center">
-      <div className="relative w-60 h-32 rounded-lg bg-zinc-700">
+    <div className="w-52 bg-white rounded-lg overflow-hidden">
+      <div className="w-full h-32 bg-sky-200">
         <img
-          className="w-full h-full object-cover overflow-hidden rounded-lg"
-          src="https://images.unsplash.com/photo-1724277228191-0cf4c80ee56f?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          className="h-full w-full object-cover object-center"
+          src={image}
           alt=""
         />
-        <span className="absolute cursor-pointer flex rounded-full bg-[#dadada8b] items-center justify-center w-8 h-8 bottom-[0%] left-1/2 -translate-x-[50%] -translate-y-[50%]">
-          <FaArrowRight size={"0.8em"} />
-        </span>
+      </div>
+      <div className="w-full p-3">
+        <h3 className="text-xl font-semibold">{name}</h3>
+        <h4 className="text-sm">{profession}</h4>
+        <button
+          onClick={handleClick}
+          className=" mt-4 px-3 py-2 text-xs text-white bg-blue-500 rounded-lg font-semibold"
+        >
+          Add Friend
+        </button>
       </div>
     </div>
   );
