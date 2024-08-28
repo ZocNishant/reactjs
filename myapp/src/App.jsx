b5d17c31cbbb60b01486fd6c84a5966dc0b6ee01
Zoc Nishant <zocnishant@gmail.com>
2024-08-28 07:15:23 +0545

    .

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -3,58 +3,10 @@ import Card from "./components/Card";
 import State from "./components/State";
 
 const App = () => {
-  const data = [
-    {
-      name: "Sarah",
-      profession: "Painter",
-      image:
-        "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      friends: false,
-    },
-    {
-      name: "Pasi",
-      profession: "Dude",
-      image:
-        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      friends: false,
-    },
-    {
-      name: "Yo Women",
-      profession: "Artist",
-      image:
-        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      friends: false,
-    },
-    {
-      name: "Tyo Women",
-      profession: "Rapper",
-      image:
-        "https://images.unsplash.com/photo-1523944339743-0fe06f079939?q=80&w=415&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      friends: false,
-    },
-  ];
-
-  const [realdata, setRealdata] = useState();
-
-  const handleFriendsButton = () => {
-    setRealdata((previousData) => {
-      return previousData.map((item, index) => {
-        if (index === 2) {
-          return { ...item, friends: !item.friends };
-        }
-        return item;
-      });
-    });
-  };
-
   return (
     <>
       {/* <Card /> */}
-      <div className="w-full h-screen bg-zinc-200 flex gap-4 items-center justify-center">
-        {realdata.map((item, index) => (
-          <State key={index} handleClick={handleFriendsButton} values={item} />
-        ))}
-      </div>
+      <State />
     </>
   );
 };
--- myapp/src/components/State.jsx
+++ myapp/src/components/State.jsx
@@ -1,28 +1,15 @@
 import React from "react";
 
-const State = ({ values, handleClick }) => {
-  const { name, profession, image } = values;
-
+const State = () => {
   return (
-    <div className="w-52 bg-white rounded-lg overflow-hidden">
-      <div className="w-full h-32 bg-sky-200">
-        <img
-          className="h-full w-full object-cover object-center"
-          src={image}
-          alt=""
-        />
+    <>
+      <div className="bg-yellow-200 h-[300px] w-[500px] flex justify-center items-center relative">
+        Hello
       </div>
-      <div className="w-full p-3">
-        <h3 className="text-xl font-semibold">{name}</h3>
-        <h4 className="text-sm">{profession}</h4>
-        <button
-          onClick={handleClick}
-          className=" mt-4 px-3 py-2 text-xs text-white bg-blue-500 rounded-lg font-semibold"
-        >
-          Add Friend
-        </button>
-      </div>
-    </div>
+      <button className="px-10 py-4 bg-red-400 absolute -mt-[28px]">
+        Click Me
+      </button>
+    </>
   );
 };
 
